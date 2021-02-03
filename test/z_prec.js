66875409879605f6c1a0fe611bbf7f224032d9c5
Julian Bruyat <bruyat.ju@live.fr>
2021-02-03 18:05:46 +0100

    (Unfinished) small examples

--- test/z_prec.js
+++ test/z_prec.js
@@ -7,6 +7,8 @@ const prec = require('../prec.js')
 const { isSubstituableGraph } = require('../graph-substitution.js');
 const assert = require('assert');
 
+const rdf = namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#", N3.DataFactory);
+
 const precNS = namespace("http://bruy.at/prec#", N3.DataFactory);
 
 
@@ -18,6 +20,11 @@ describe("prec", function() {
                 const expected = new N3.Store(
                     new N3.Parser().parse(fs.readFileSync(testFolder + file, "utf-8"))
                 );
+
+                if (expected.getQuads(precNS.testMetaData, precNS.kind, N3.DataFactory.literal("SmallExamples")), N3.DataFactory.defaultGraph()) {
+                    smallExample(expected);
+                    return;
+                }
 
                 const meta = expected.getQuads(precNS.testMetaData);
                 assert(meta.length === 3);
@@ -44,3 +51,28 @@ describe("prec", function() {
     }
 });
 
+
+function get(store, subject, predicate) {
+    const quads = store.getQuads(subject, predicate, null, N3.DataFactory.defaultGraph());
+
+    if (quads.length != 1) return null;
+    else return quads[0].object;
+}
+
+function smallExample(store) {
+    for (const unitTest of store.getQuads(null, rdf.type, precNS.unitTest)) {
+        const node = unitTest.subject;
+
+        const context       = get(store, node, precNS.context);
+        const output        = get(store, node, precNS.output);
+        const propertyGraph = get(store, node, precNS.propertyGraph);
+
+        assert.notStrictEqual(context, null);
+        assert.notStrictEqual(output, null);
+        assert.notStrictEqual(propertyGraph, null);
+
+        // TODO
+
+
+    }
+}
