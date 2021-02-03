1821fc0c5a50f6a935ad708ee2f1ebf175bc5927
Julian Bruyat <bruyat.ju@live.fr>
2021-02-03 22:44:30 +0100

    Remove file-read.js

--- prec.js
+++ prec.js
@@ -10,7 +10,42 @@ const N3            = require('n3');
 
 const RDFGraphBuilder = require("./prec3/graph-builder.js");
 const graphReducer    = require("./prec3/graph-reducer.js");
-const fileReader      = require("./file-read.js");
+
+const fs = require('fs');
+
+/// Returns the content of filename, line by line
+function fileToString(filename) {
+    if (format === "JSON") {
+        return filename;
+    }
+    
+    return fs.readFileSync(filename, 'utf-8').split(/\r?\n/);
+}
+
+/// Transforms an array of strings, each strings being a JSON representation,
+/// into an array of JS objects
+function stringsToJsObjects(file_content) {
+    let collection = [];
+
+    for (const line of file_content) {
+        if (line.trim() == "") {
+            continue;
+        }
+
+        collection.push(JSON.parse(line));
+    }
+
+    return collection;
+}
+
+const fileReader = {
+    "fromNeo4j": filename => stringsToJsObjects(fileToString(filename)),
+    "fromNeo4jString": content => stringsToJsObjects(content.split(/\r?\n/))
+};
+
+
+
+
 
 
 function filenameToArrayOfQuads(filename) {
