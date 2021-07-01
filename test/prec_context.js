b08b7cb1a0293588e3e367b47855330461f9b9e2
Julian Bruyat <bruyat.ju@live.fr>
2021-07-01 17:30:16 +0200

    Rewrite on the edge transformation related unit tests

--- test/prec_context.js
+++ test/prec_context.js
@@ -1,152 +1,26 @@
-
 const utility = require("./utility.js");
 const graphReducer = require("../src/prec/graph-reducer.js");
 const assert = require('assert');
 const { isomorphic } = require("rdf-isomorphic");
 const precUtils = require('../src/rdf/utils')
 
-
-const basicGraphs = {
-    oneEdge:  `
-        :edge  a pgo:Edge ; rdf:subject :s  ; rdf:predicate :p  ; rdf:object :o  .
-    `,
-    twoEdges: `
-        :edge1 a pgo:Edge ; rdf:subject :s1 ; rdf:predicate :p1 ; rdf:object :o1 .
-        :edge2 a pgo:Edge ; rdf:subject :s2 ; rdf:predicate :p2 ; rdf:object :o2 .
-    `,
-    oneEdgeType:  `
-        :edge  a pgo:Edge ; rdf:subject :s  ; rdf:predicate :p  ; rdf:object :o  .
-        :p rdfs:label "type1" .
-    `,
-    edgeDiff: `
-        :edge1 a pgo:Edge ; rdf:subject :s1 ; rdf:predicate :p1 ; rdf:object :o1 .
-        :edge2 a pgo:Edge ; rdf:subject :s2 ; rdf:predicate :p2 ; rdf:object :o2 .
-        
-        :p1 rdfs:label "type1" .
-        :p2 rdfs:label "type2" .
-    `,
-    differentSourceLabel: `
-        :edge1 a pgo:Edge ; rdf:subject :person ; rdf:predicate :p ; rdf:object :o .
-        :edge2 a pgo:Edge ; rdf:subject :animal ; rdf:predicate :p ; rdf:object :o .
-        :p rdfs:label "Predicate" .
-
-        :person a [ rdfs:label "Person" ] .
-        :animal a [ rdfs:label "Animal" ] .
-    `,
-    oneEdgeWith1Property: `
-        :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
-            :propKey1 :propValue1 .
-    `,
-    oneEdgeWith2Properties: `
-        :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
-            :propKey1 :propValue1 ; :propKey2 :propValue2 .
-    `,
-    edgeWithMetaProperty: `
-        :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
-            :edge :propKey :propValue .
-        
-        << :edge :propKey :propValue >> :metaPropKey :metaPropValue .
-    `
-};
-
-const contexts = {
-    emptyContext  : ``,
-    allUnique     : `prec:Edges prec:templatedBy prec:RdfStarUnique . `,
-    allOccurences : `prec:Edges prec:templatedBy prec:RdfStarOccurrence . `,
-    type1specialization: `
-        prec:Edges prec:templatedBy prec:RdfStarUnique .
-        :type1 prec:IRIOfEdgeLabel "type1" .
-    `,
-    type1specializationBN: `
-        prec:Edges prec:templatedBy prec:RdfStarUnique .
-        [] a prec:EdgeRule ;
-            prec:edgeIRI   :type1 ;
-            prec:edgeLabel "type1" .
-    `,
-    type1templatedBy: `
-        prec:Edges prec:templatedBy prec:RdfStarOccurrence .
-        [] a prec:EdgeRule ;
-            prec:edgeIRI   :type1 ;
-            prec:edgeLabel "type1" ;
-            prec:templatedBy prec:RdfStarUnique .
-    `,
-    predicateOnPerson: `
-        prec:Edges prec:templatedBy prec:RdfStarOccurrence .
-        [] a prec:EdgeRule ;
-            prec:edgeIRI :NewPredicate ;
-            prec:templatedBy prec:RdfStarUnique ;
-            prec:edgeLabel "Predicate" ;
-            prec:sourceLabel "Person"
-        .
-    `,
-    bothSpecialization: `
-        [] a prec:EdgeRule ;
-            prec:edgeIRI :type1 ;
-            prec:edgeLabel "type1" ;
-            prec:templatedBy prec:RdfStarUnique
-        .
-        
-        [] a prec:EdgeRule ;
-            prec:edgeIRI :type2 ;
-            prec:edgeLabel "type2" ;
-            prec:templatedBy prec:RdfStarUnique
-        .
-    `,
-
-
-    useRdfStarrenameTermsImplicit: `
-        prec:Edges prec:subject :source ;
-            prec:predicate :label ;
-            prec:object :target .
-    `,
-    templatedByPG: `
-        prec:Edges prec:templatedBy prec:RDFReification ;
-            prec:subject :source ;
-            prec:predicate :label ;
-            prec:object :target .
-    `,
-    templatedByCustom: `
-        prec:Edges prec:templatedBy [
-            prec:composedOf << rdf:subject rdf:predicate rdf:object >> ,
-                << pvar:destination pvar:edgeIRI pvar:source >>
-        ] .
-    `,
-    templatedByCustomWithRenaming: `
-        prec:Edges prec:templatedBy [
-            prec:composedOf << rdf:subject rdf:predicate rdf:object >> ,
-                << pvar:destination pvar:edgeIRI pvar:source >>
-        ] ;
-            prec:subject :source ;
-            prec:predicate :label ;
-            prec:object :target .
-    `,
-    swapSO: `
-        prec:Edges prec:subject rdf:object ; prec:object rdf:subject .
-    `,
-    templateWithLabel: `
-        prec:Edges prec:templatedBy [
-            prec:composedOf << :anEdge :holdsTheLabel pvar:label  >>
-        ] .
-    `
-}
-
-function badToColorizedToString(quads, match, indent) {
-    let asString = precUtils.badToString(quads, indent).split(/\r?\n/);
-
-    for (let i = 0 ; i != quads.length ; ++i) {
-        if (match[i] === undefined) continue;
-
-        if (match[i] >= 0) asString[i] = "\x1b[36m" + asString[i] + "\x1b[0m";
+function badToColorizedToStrings(quads1, quads2, indent = 8) {
+    function badToColorizedToString(quads, match, indent) {
+        let asString = precUtils.badToString(quads, indent).split(/\r?\n/);
+    
+        for (let i = 0 ; i != quads.length ; ++i) {
+            if (match[i] === undefined) continue;
+    
+            if (match[i] >= 0) asString[i] = "\x1b[36m" + asString[i] + "\x1b[0m";
+        }
+    
+        return asString.join("\n");
     }
 
-    return asString.join("\n");
-}
-
-function badToColorizedToStrings(quads1, quads2) {
     let [s1, s2] = precUtils.approximateIsomorphism(quads1, quads2)
     return [
-        badToColorizedToString(quads1, s1, 8),
-        badToColorizedToString(quads2, s2, 8)
+        badToColorizedToString(quads1, s1, indent),
+        badToColorizedToString(quads2, s2, indent)
     ];
 }
 
@@ -165,18 +39,6 @@ function print(store, d1, graphName, d2, contextName, expectedStore) {
     console.error(expected);
 }
 
-function runATest(graphName, contextName, expected) {
-    it(graphName + " x " + contextName, function() {
-        const store         = utility.turtleToDStar(basicGraphs[graphName]);
-        const context       = utility.turtleToQuads(contexts[contextName]);
-        graphReducer(store, context);
-
-        const expectedStore = utility.turtleToDStar(expected);
-        const r = isomorphic(store.getQuads(), expectedStore.getQuads());
-        if (!r) print(store, basicGraphs, graphName, contexts, contextName, expectedStore);
-        assert.ok(r);
-    });
-}
 
 
 function runATest_(dict, graphName, contextName, expected) {
@@ -200,209 +62,29 @@ function test(name, source, context, expected) {
 
         const expectedStore = utility.turtleToDStar(expected);
         const r = isomorphic(store.getQuads(), expectedStore.getQuads());
+        let msg = "";
         if (!r) {
-            console.error("Error on " + name);
-            console.error("• Base Graph:");
-            console.error(source);
-            console.error("• Context:");
-            console.error(context);
+            msg = "Error on " + name;
+            msg += '\n' + '\x1b[0m' + "• Base Graph:";
+            msg += '\n' + source;
+            msg += '\n' + "• Context:";
+            msg += '\n' + context;
         
-            [result, expected] = badToColorizedToStrings(store.getQuads(), expectedStore.getQuads());
+            [result, expected] = badToColorizedToStrings(store.getQuads(), expectedStore.getQuads(), 2);
         
-            console.error(`• Result (${store.size} quads):`);
-            console.error(result);
-            console.error(`• Expected (${expectedStore.size} quads):`);
-            console.error(expected);
+            msg += '\n' + `• Result (${store.size} quads):`;
+            msg += '\n' + result;
+            msg += '\n' + `• Expected (${expectedStore.size} quads):`;
+            msg += '\n' + expected;
         }
         
-        assert.ok(r);
+        assert.ok(r, msg);
     });
 }
 
-describe("Edge convertion", function () {
-    describe('Lack of context', function() {
-        runATest("oneEdge", "emptyContext", basicGraphs['oneEdge']);
-        runATest("twoEdges", "emptyContext", basicGraphs['twoEdges']);
-        runATest("oneEdgeType", "emptyContext", basicGraphs['oneEdgeType']);
-        runATest("edgeDiff", "emptyContext", basicGraphs['edgeDiff']);
-        runATest("differentSourceLabel", "emptyContext", basicGraphs['differentSourceLabel']);
-    })
+describe('Context Applier', function () {
+  require('./prec_impl/rules-for-edges.test')(test);
 
-    describe("Simple graphs templatedBy", function() {
-        runATest("oneEdge", "allUnique",
-            `
-                << :s :p :o  >> a pgo:Edge .
-                :s :p :o .
-            `
-        );
-    
-        runATest("twoEdges", "allUnique",
-            `
-                << :s1 :p1 :o1  >> a pgo:Edge .
-                :s1 :p1 :o1 .
-                << :s2 :p2 :o2  >> a pgo:Edge .
-                :s2 :p2 :o2 .
-            `
-        );
-
-        runATest("oneEdge", "allOccurences",
-            `
-                :edge a pgo:Edge .
-                :edge prec:occurrenceOf << :s :p :o  >> .
-            `
-        );
-
-        runATest("oneEdgeType", "type1specialization",
-            `
-                << :s :type1 :o >> a pgo:Edge .
-                :s :type1 :o .
-            `
-        );
-
-        runATest("oneEdgeType", "type1templatedBy",
-            `
-                << :s :type1 :o >> a pgo:Edge .
-                :s :type1 :o .
-            `
-        );
-
-        runATest("edgeDiff", "type1specialization",
-            `
-                << :s1 :type1 :o1  >> a pgo:Edge .
-                :s1 :type1 :o1 .
-
-                << :s2 :p2 :o2  >> a pgo:Edge .
-                :s2 :p2 :o2 .
-                :p2 rdfs:label "type2" .
-            `
-        );
-    
-        runATest("edgeDiff", "type1specializationBN",
-            `
-                << :s1 :type1 :o1  >> a pgo:Edge .
-                :s1 :type1 :o1 .
-
-                << :s2 :p2 :o2  >> a pgo:Edge .
-                :s2 :p2 :o2 .
-                :p2 rdfs:label "type2" .
-            `
-        );
-    
-        runATest("edgeDiff", "type1templatedBy",
-            `
-                << :s1 :type1 :o1  >> a pgo:Edge .
-                :s1 :type1 :o1 .
-
-                :edge2 a pgo:Edge .
-                :edge2 prec:occurrenceOf << :s2 :p2 :o2  >> .
-                :p2 rdfs:label "type2" .
-            `
-        );
-
-        runATest("differentSourceLabel", "predicateOnPerson",
-            `
-                << :person :NewPredicate :o  >> a pgo:Edge .
-                :person :NewPredicate :o .
-
-                :edge2 a pgo:Edge .
-                :edge2 prec:occurrenceOf << :animal :p :o  >> .
-                :p rdfs:label "Predicate" .
-
-                :person a [ rdfs:label "Person" ] .
-                :animal a [ rdfs:label "Animal" ] .
-            `
-        );
-
-        runATest("edgeDiff", "bothSpecialization",
-            `
-                << :s1 :type1 :o1  >> a pgo:Edge .
-                :s1 :type1 :o1 .
-        
-                << :s2 :type2 :o2  >> a pgo:Edge .
-                :s2 :type2 :o2 .
-            `
-        );
-        
-        runATest("oneEdge", "templatedByPG",
-            `
-            :edge a pgo:Edge ;
-            :source :s ;
-            :label  :p ;
-            :target :o .
-            `
-        );
-        
-        runATest("oneEdge", "templatedByCustom",
-            `
-                rdf:subject rdf:predicate rdf:object .
-                :o :p :s .
-            `
-        );
-        
-        runATest("oneEdge", "templatedByCustomWithRenaming",
-            `
-                :source :label :target .
-                :o :p :s .
-            `
-        );
-
-        runATest("oneEdge", "useRdfStarrenameTermsImplicit",
-            `
-                :edge a pgo:Edge ;
-                :source :s ;
-                :label  :p ;
-                :target :o .
-            `
-        );
-
-        // Subsitutions should be operated at the same time
-        // - In a real application, this test checks if it is possible to revert the direction of every edge.
-        runATest("oneEdge", "swapSO",
-            `
-                :edge  a pgo:Edge ; rdf:object :s  ; rdf:predicate :p  ; rdf:subject :o  .
-            `
-        );
-        
-        // Labels
-        runATest("edgeDiff", "templateWithLabel",
-        `
-            :anEdge :holdsTheLabel "type1" .
-            :anEdge :holdsTheLabel "type2" .
-        `
-        )
-    })
-
-    describe("Meta properties", function() {
-        runATest("oneEdgeWith1Property"  , "emptyContext", basicGraphs["oneEdgeWith1Property"]  );
-        runATest("oneEdgeWith2Properties", "emptyContext", basicGraphs["oneEdgeWith2Properties"]);
-
-        runATest("oneEdgeWith1Property", "allUnique", 
-            `
-                :s :p :o .
-                << :s :p :o >> a pgo:Edge ; :propKey1 :propValue1 .
-            `
-        );
-
-        runATest("oneEdgeWith2Properties", "allUnique", 
-            `
-                :s :p :o .
-                << :s :p :o >> a pgo:Edge ; :propKey1 :propValue1 ; :propKey2 :propValue2 .
-            `
-        );
-
-        /*
-        This test currently can not pass as N3.Store does not support multi
-        nested RDF Quads
-
-        runATest("edgeWithMetaProperty", "allUnique", 
-            `
-            :s :p :o .
-            << :s :p :o >> a pgo:Edge ; :propKey :propValue .
-            << << :s :p :o >> :propKey :propValue >> :metaPropKey :metaPropValue .
-            `
-        );
-        */
-    })
 });
 
 describe("Property convertion", function() {
---
+++ test/prec_impl/rules-for-edges.test.js
@@ -0,0 +1,284 @@
+
+module.exports = function (test) {
+  describe("Edge convertion", function () {
+    const basicGraphs = {
+      oneEdge: `
+          :edge  a pgo:Edge ; rdf:subject :s  ; rdf:predicate :p  ; rdf:object :o  .
+      `,
+      twoEdges: `
+          :edge1 a pgo:Edge ; rdf:subject :s1 ; rdf:predicate :p1 ; rdf:object :o1 .
+          :edge2 a pgo:Edge ; rdf:subject :s2 ; rdf:predicate :p2 ; rdf:object :o2 .
+      `,
+      oneEdgeType: `
+          :edge  a pgo:Edge ; rdf:subject :s  ; rdf:predicate :p  ; rdf:object :o  .
+          :p rdfs:label "type1" .
+      `,
+      edgeDiff: `
+          :edge1 a pgo:Edge ; rdf:subject :s1 ; rdf:predicate :p1 ; rdf:object :o1 .
+          :edge2 a pgo:Edge ; rdf:subject :s2 ; rdf:predicate :p2 ; rdf:object :o2 .
+          
+          :p1 rdfs:label "type1" .
+          :p2 rdfs:label "type2" .
+      `,
+      differentSourceLabel: `
+          :edge1 a pgo:Edge ; rdf:subject :person ; rdf:predicate :p ; rdf:object :o .
+          :edge2 a pgo:Edge ; rdf:subject :animal ; rdf:predicate :p ; rdf:object :o .
+          :p rdfs:label "Predicate" .
+
+          :person a [ rdfs:label "Person" ] .
+          :animal a [ rdfs:label "Animal" ] .
+      `,
+      oneEdgeWith1Property: `
+          :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
+              :propKey1 :propValue1 .
+      `,
+      oneEdgeWith2Properties: `
+          :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
+              :propKey1 :propValue1 ; :propKey2 :propValue2 .
+      `,
+      edgeWithMetaProperty: `
+          :edge a pgo:Edge ; rdf:subject :s ; rdf:predicate :p ; rdf:object :o ;
+              :propKey :propValue .
+          
+          << :edge :propKey :propValue >> :metaPropKey :metaPropValue .
+      `
+    };
+
+    const contexts = {
+      emptyContext  : ``,
+      allUnique     : `prec:Edges prec:templatedBy prec:RdfStarUnique . `,
+      allOccurences : `prec:Edges prec:templatedBy prec:RdfStarOccurrence . `,
+      type1specialization: `
+          prec:Edges prec:templatedBy prec:RdfStarUnique .
+          :type1 prec:IRIOfEdgeLabel "type1" .
+      `,
+      type1templatedBy: `
+          prec:Edges prec:templatedBy prec:RdfStarOccurrence .
+          [] a prec:EdgeRule ;
+              prec:edgeIRI   :type1 ;
+              prec:edgeLabel "type1" ;
+              prec:templatedBy prec:RdfStarUnique .
+      `,
+    }
+
+    describe('Lack of context', function () {
+      const graphs = [
+        'oneEdge',
+        'twoEdges',
+        'oneEdgeType',
+        'edgeDiff',
+        'differentSourceLabel',
+        'oneEdgeWith1Property',
+        'oneEdgeWith2Properties',
+        'edgeWithMetaProperty'
+      ];
+
+      for (const graph of graphs) {
+        test('Idempotency on basicGraphs.' + graph, basicGraphs[graph], '', basicGraphs[graph]);
+      }
+    });
+
+    describe("PG without properties", function () {
+      test('OneEdge with prec:RdfStarUnique',
+        basicGraphs.oneEdge, contexts.allUnique,
+        ':s :p :o . << :s :p :o  >> a pgo:Edge .'
+      );
+
+      test('TwoEdges with prec:RdfStarUnique',
+        basicGraphs.twoEdges, contexts.allUnique, 
+        `:s1 :p1 :o1 . << :s1 :p1 :o1  >> a pgo:Edge .
+        :s2 :p2 :o2 . << :s2 :p2 :o2  >> a pgo:Edge .`
+      );
+
+      test('OneEdge with prec:RdfStarOccurrence',
+        basicGraphs.oneEdge, contexts.allOccurences,
+        ` :edge a pgo:Edge . :edge prec:occurrenceOf << :s :p :o  >> .`
+      );
+
+      test('One typed edge with a rule',
+        basicGraphs.oneEdgeType, contexts.type1specialization,
+        ':s :type1 :o . << :s :type1 :o >> a pgo:Edge .'
+      );
+
+      test('One type edge with a specific template for it',
+        basicGraphs.oneEdgeType, contexts.type1templatedBy,
+        ':s :type1 :o . << :s :type1 :o >> a pgo:Edge .'
+      );
+
+      test('Two typed edges with one that is unaffected by the implicit rule',
+        basicGraphs.edgeDiff, contexts.type1specialization,
+        `:s1 :type1 :o1 . << :s1 :type1 :o1  >> a pgo:Edge .
+
+        :s2 :p2 :o2 . << :s2 :p2 :o2  >> a pgo:Edge .
+        :p2 rdfs:label "type2" .`
+      );
+
+      test('Two typed edges with one that is unaffected by the explicit rule',
+        basicGraphs.edgeDiff,
+        `
+          prec:Edges prec:templatedBy prec:RdfStarUnique .
+          [] a prec:EdgeRule ; prec:edgeIRI :type1 ; prec:edgeLabel "type1" .
+        `,
+        `:s1 :type1 :o1 . << :s1 :type1 :o1  >> a pgo:Edge .
+
+        :s2 :p2 :o2 . << :s2 :p2 :o2  >> a pgo:Edge .
+        :p2 rdfs:label "type2" .`
+      );
+      
+      test('Two typed edges with only one affected by a rule with template',
+        basicGraphs.edgeDiff, contexts.type1templatedBy,
+        `:s1 :type1 :o1 . << :s1 :type1 :o1  >> a pgo:Edge .
+
+          :edge2 a pgo:Edge .
+          :edge2 prec:occurrenceOf << :s2 :p2 :o2  >> .
+          :p2 rdfs:label "type2" .`
+      );
+
+      test('A rule that targets edges with a specific souce node label',
+        basicGraphs.differentSourceLabel,
+        `
+          prec:Edges prec:templatedBy prec:RdfStarOccurrence .
+          [] a prec:EdgeRule ;
+              prec:edgeIRI :NewPredicate ;
+              prec:templatedBy prec:RdfStarUnique ;
+              prec:edgeLabel "Predicate" ;
+              prec:sourceLabel "Person"
+          .
+        `,
+        `
+          << :person :NewPredicate :o  >> a pgo:Edge .
+          :person :NewPredicate :o .
+
+          :edge2 a pgo:Edge .
+          :edge2 prec:occurrenceOf << :animal :p :o  >> .
+          :p rdfs:label "Predicate" .
+
+          :person a [ rdfs:label "Person" ] .
+          :animal a [ rdfs:label "Animal" ] .
+        `
+      );
+
+      test('A rule for the two edges in the graph',
+        basicGraphs.edgeDiff,
+        `
+        [] a prec:EdgeRule ;
+          prec:edgeIRI :type1 ;
+          prec:edgeLabel "type1" ;
+          prec:templatedBy prec:RdfStarUnique
+        .
+        
+        [] a prec:EdgeRule ;
+          prec:edgeIRI :type2 ;
+          prec:edgeLabel "type2" ;
+          prec:templatedBy prec:RdfStarUnique
+        .
+        `,
+        `
+          << :s1 :type1 :o1  >> a pgo:Edge .
+          :s1 :type1 :o1 .
+
+          << :s2 :type2 :o2  >> a pgo:Edge .
+          :s2 :type2 :o2 .
+        `
+      );
+          
+      test('Substitution within the RDFReification template',
+        basicGraphs.oneEdge,
+        `prec:Edges prec:templatedBy prec:RDFReification ;
+          prec:subject :source ;
+          prec:predicate :label ;
+          prec:object :target .`,
+        `:edge a pgo:Edge ;
+          :source :s ;
+          :label  :p ;
+          :target :o .`
+      );
+          
+      test("One edge with an user defined template", 
+        basicGraphs.oneEdge,    
+        `prec:Edges prec:templatedBy [ prec:composedOf
+            << rdf:subject rdf:predicate rdf:object >> ,
+            << pvar:destination pvar:edgeIRI pvar:source >>
+        ] .`,
+        'rdf:subject rdf:predicate rdf:object . :o :p :s . '
+      );
+        
+      test("One edge with an user defined template and with substitutions",
+        basicGraphs.oneEdge,
+        `prec:Edges prec:templatedBy [ prec:composedOf
+          << rdf:subject rdf:predicate rdf:object >> ,
+          << pvar:destination pvar:edgeIRI pvar:source >>
+        ] ;
+          prec:subject   :source ;
+          prec:predicate :label ;
+          prec:object    :target .
+        `,
+        ':source :label :target . :o :p :s .'
+      );
+
+      test("One edge with implicit renaming of the RDF Reification model",
+        basicGraphs.oneEdge,
+        `
+          prec:Edges
+            prec:subject   :source ;
+            prec:predicate :label ;
+            prec:object    :target .
+        `,
+        `
+          :edge a pgo:Edge ;
+          :source :s ;
+          :label  :p ;
+          :target :o .
+        `
+      );
+
+      test('Change the direction of every edge with substitutions',
+        basicGraphs.oneEdge,
+        'prec:Edges prec:subject rdf:object ; prec:object rdf:subject .',
+        ':edge a pgo:Edge ; rdf:object :s ; rdf:predicate :p ; rdf:subject :o .'
+      );
+      
+      test('Use the label in an edge template',
+        basicGraphs.edgeDiff,
+        `
+        prec:Edges prec:templatedBy [ prec:composedOf
+          << :anEdge :holdsTheLabel pvar:label  >>
+        ] .
+        `,
+        ':anEdge :holdsTheLabel "type1", "type2" .'
+      );
+    })
+
+    describe("PG with properties on edges", function () {
+      test('One edge with one property',
+        basicGraphs.oneEdgeWith1Property,
+        contexts.allUnique,
+        `
+          :s :p :o .
+          << :s :p :o >> a pgo:Edge ; :propKey1 :propValue1 .
+        `
+      );
+
+      test("One edge with two properties",
+        basicGraphs.oneEdgeWith2Properties,
+        contexts.allUnique, 
+        `
+          :s :p :o .
+          << :s :p :o >> a pgo:Edge ; 
+            :propKey1 :propValue1 ;
+            :propKey2 :propValue2 .
+        `
+      );
+
+      test("One edge with one property with one meta property",
+        basicGraphs.edgeWithMetaProperty,
+        contexts.allUnique,
+        `
+          :s :p :o .
+          << :s :p :o >> a pgo:Edge ; :propKey :propValue .
+          << << :s :p :o >> :propKey :propValue >> :metaPropKey :metaPropValue .
+        `
+      );
+    });
+  });
+};
