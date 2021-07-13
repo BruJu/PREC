3ca09e0991e8ebbee45c55e5645343c51bb37629
Julian Bruyat <bruyat.ju@live.fr>
2021-07-13 15:20:56 +0200

    RuleType.d.ts

---
+++ src/prec/RuleType.d.ts
@@ -0,0 +1,24 @@
+import { Term } from "rdf-js";
+
+/**
+ * The list of terms that are related to rules of a type.
+ */
+export type RuleDomain = {
+  /** The type of the rules */
+  readonly RuleType          : Term;
+  /** The default template for the data affected by this rule */
+  readonly DefaultTemplate   : Term;
+  /** The predicate to address the label of the element */
+  readonly MainLabel         : Term;
+  /** The possible conditions */
+  readonly PossibleConditions: Term[];
+  /**
+   * The list of domain nodes, paired with the list of conditions they are
+   * incompatible with
+   */
+  readonly TemplateBases     : [Term, Term[]][]
+  /** The term used for short rules (`:name prec:IRIOFProperty "Name".`) */
+  readonly ShortcutIRI       : Term;
+  /** The substitution term for the label */
+  readonly SubstitutionTerm  : Term;
+}
