0bff1948e72f2818bf3036e11992b5df4d48fe04
Julian Bruyat <bruyat.ju@live.fr>
2021-09-09 09:07:29 +0200

    PRSC edge

--- src/prec-c-prsc/PrscContext.ts
+++ src/prec-c-prsc/PrscContext.ts
@@ -1,65 +1,126 @@
-import { Quad, Quad_Object, Quad_Predicate, Quad_Subject, Term } from "@rdfjs/types";
+import { DatasetCore, Quad, Quad_Object, Quad_Predicate, Quad_Subject, Term, NamedNode } from "@rdfjs/types";
 import { DataFactory } from "n3";
 import DStar from "../dataset";
 
-const $quad = DataFactory.quad;
-const $variable = DataFactory.variable;
+const $quad         = DataFactory.quad;
+const $literal      = DataFactory.literal;
+const $variable     = DataFactory.variable;
 const $defaultGraph = DataFactory.defaultGraph();
-const $literal = DataFactory.literal;
 
 import namespace from '@rdfjs/namespace';
+import { followThrough } from "../rdf/path-travelling";
 const rdf  = namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#", { factory: DataFactory });
 const rdfs = namespace("http://www.w3.org/2000/01/rdf-schema#"      , { factory: DataFactory });
 const pgo  = namespace("http://ii.uwb.edu.pl/pgo#"                  , { factory: DataFactory });
 const prec = namespace("http://bruy.at/prec#"                       , { factory: DataFactory });
 const pvar = namespace("http://bruy.at/prec-trans#"                 , { factory: DataFactory });
 
+////////////////////////////////////////////////////////////////////////////////
+// Path following
 
-interface SchemaDetector {
-  get iri(): Quad_Subject;
-  match(dataset: DStar, element: Quad_Subject): boolean;
-  produce(destination: DStar, source: DStar, element: Quad_Subject): void;
+function followOrNull(dataset: DatasetCore): Quad_Object | null {
+  const triples = [...dataset];
+  if (triples.length === 0) return null;
+  else if (triples.length === 1) return triples[0].object;
+  else throw Error("More than one path");
 }
 
-class NodeSchemaDetector implements SchemaDetector {
+function followAll(dataset: DatasetCore, subject: Quad_Subject, predicate: Quad_Predicate): Quad_Object[] {
+  return [...dataset.match(subject, predicate, null, $defaultGraph)].map(quad => quad.object);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// General purpose utilty functions
+
+/**
+ * Returns true if lhs and rhs contains the same strings.
+ * 
+ * Assumes that both array contains no duplicate.
+ */
+function haveSameStrings(lhs: string[], rhs: string[]): boolean {
+  if (lhs.length !== rhs.length) return false;
+
+  for (const label of lhs) {
+    if (!rhs.includes(label)) return false;
+  }
+
+  return true;
+}
+
+// TODO: finish this function (non mandatory values should really be non mandatory)
+function haveSameProperties(
+  actual: string[],
+  expected: { name: string, mandatory: boolean }[]
+): boolean {
+  if (actual.length !== expected.length) return false;
+
+  for (const property of actual) {
+    if (expected.find(p => p.name === property) === undefined) return false;
+  }
+
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Schema detection
+
+abstract class SchemaDetector {
   contextGraph: DStar;
   myName: Quad_Subject;
-  labels: string[];
-  properties: { name: string, mandatory: boolean }[];
   composition: Quad[];
 
   constructor(dataset: DStar, myName: Quad_Subject) {
     this.contextGraph = dataset;
     this.myName = myName;
-    this.labels = [...dataset.match(myName, prec.nodeLabel, null, $defaultGraph)].map(term => term.object.value);
-    this.properties = [...dataset.match(myName, prec.propertyName, null, $defaultGraph)].map(term => ({ name: term.object.value, mandatory: true }));
     this.composition = [...dataset.match(myName, prec.composedOf, null, $defaultGraph)].map(term => term.object) as Quad[];
   }
-
+  
+  /** The IRI of the schema */
   get iri(): Quad_Subject { return this.myName; }
 
-  match(dataset: DStar, element: Quad_Subject): boolean {
-    const { labels, properties } = NodeSchemaDetector.describePREC0Node(dataset, element);
+  /** Returns true if the given element complies with this schema */
+  abstract match(dataset: DStar, element: Quad_Subject): boolean;
 
-    if (labels.length === this.labels.length) {
-      for (const label of labels) {
-        if (!this.labels.includes(label)) return false;
-      }
-    }
-
-    if (properties.length === this.properties.length) {
-      for (const property of properties) {
-        if (this.properties.find(p => p.name === property) === undefined) return false;
-      }
-    }
-
-    return true;
-  }
-
+  /** Produce the new representation of the thing, we suppose than match == true */
   produce(destination: DStar, source: DStar, element: Quad_Subject): void {
     for (const quadToAdd of this.composition) {
       destination.add(this.instanciate(source, element, quadToAdd));
     }
+  }
+
+  abstract instanciate(source: DStar, element: Quad_Subject, quadToAdd: Quad): Quad;
+}
+
+function extractPropertyNameLabels(dataset: DStar, pgNodeOrEdge: Quad_Subject) {
+  return dataset.matchAndBind([
+    $quad(pgNodeOrEdge, $variable('propertyName'), $variable('blankNode')),
+    $quad($variable('propertyName'), rdfs.label, $variable('propertyNameLabel'))
+  ]).map(binding => binding.propertyNameLabel as Term).map(propertyNameLabel => propertyNameLabel.value);
+}
+
+class NodeSchemaDetector extends SchemaDetector {
+  labels: string[];
+  properties: { name: string, mandatory: boolean }[];
+  
+  constructor(dataset: DStar, myName: Quad_Subject) {
+    super(dataset, myName);
+    this.labels = followAll(dataset, myName, prec.nodeLabel).map(labelLiteral => labelLiteral.value);
+    this.properties = followAll(dataset, myName, prec.propertyName).map(object => ({ name: object.value, mandatory: true }));
+  }
+
+  match(dataset: DStar, node: Quad_Subject): boolean {
+    const labels = dataset.matchAndBind([
+      $quad(node, rdf.type, $variable('type')),
+      $quad($variable('type'), rdfs.label, $variable('label'))
+    ]).map(binding => binding.label as Term).map(term => term.value);
+    if (!haveSameStrings(labels, this.labels)) return false;
+    
+    const properties = extractPropertyNameLabels(dataset, node);
+    if (!haveSameProperties(properties, this.properties)) return false;
+
+    return true;
   }
 
   instanciate(source: DStar, element: Quad_Subject, quadToAdd: Quad): Quad {
@@ -74,25 +135,7 @@ class NodeSchemaDetector implements SchemaDetector {
 
       if (term.equals(pvar.node)) return element;
       if (term.termType === 'BlankNode') {
-        const triples = this.contextGraph.match(term);
-        if (triples.size !== 1) throw Error('Blank node is not unique ; ' + triples.size);
-        const triple = [...triples][0];
-        if (triple.predicate.equals(prec.prsc_valueOf)) {
-          if (triple.object.termType !== 'Literal')
-            throw Error('Object is not a literal');
-
-          const propertyName = triple.object.value;
-          const bindings = source.matchAndBind([
-            $quad(element, $variable('propertyKey'), $variable('propertyBN')),
-            $quad($variable('propertyKey'), rdfs.label, $literal(propertyName)),
-            $quad($variable('propertyBN'), rdf.value, $variable('propertyValue'))
-          ]).map(bindings => bindings.propertyValue as Term);
-          
-          if (bindings.length !== 1) throw Error('Not 1 value');
-          return bindings[0];
-        } else {
-          throw Error('Found bad blank node');
-        }
+        return instanciateBlankNode(this.contextGraph, source, term, element);
       }
       
       return term;
@@ -100,27 +143,100 @@ class NodeSchemaDetector implements SchemaDetector {
 
     return transform(quadToAdd) as Quad;
   }
+}
 
-  static describePREC0Node(dataset: DStar, node: Quad_Subject) {
-    const result: { labels: string[], properties: string[] } = { labels: [], properties: [] };
 
-    result.labels = dataset.matchAndBind([
-      $quad(node, rdf.type, $variable('type')),
-      $quad($variable('type'), rdfs.label, $variable('label'))
-    ]).map(binding => binding.label as Term).map(term => term.value);
+class EdgeSchemaDetector extends SchemaDetector {
+  labels: string[];
+  properties: { name: string, mandatory: boolean }[];
 
-    result.properties = dataset.matchAndBind([
-      $quad(node, $variable('propertyName'), $variable('blankNode')),
-      $quad($variable('propertyName'), rdfs.label, $variable('propertyNameLabel'))
-    ]).map(binding => binding.propertyNameLabel as Term).map(propertyNameLabel => propertyNameLabel.value);
+  startForm: Quad_Subject | null;
+  endForm: Quad_Subject | null;
 
-    return result;
+  constructor(dataset: DStar, myName: Quad_Subject) {
+    super(dataset, myName);
+
+    this.labels = followAll(dataset, myName, prec.edgeLabel).map(labelLiteral => labelLiteral.value);
+    this.properties = followAll(dataset, myName, prec.propertyName).map(object => ({ name: object.value, mandatory: true }));
+
+    this.startForm = followOrNull(dataset.match(myName, prec.prscSource, null, $defaultGraph)) as (Quad_Subject | null);
+    this.endForm = followOrNull(dataset.match(myName, prec.prscDestination, null, $defaultGraph)) as (Quad_Subject | null);
+  }
+
+  match(dataset: DStar, element: Quad_Subject): boolean {
+    if (!EdgeSchemaDetector.nodeCompliesWith(dataset, element, rdf.subject, this.startForm)
+    || !EdgeSchemaDetector.nodeCompliesWith(dataset, element, rdf.object, this.endForm)) {
+      return false;
+    }
+    
+    const labels = dataset.matchAndBind([
+      $quad(element, rdf.predicate, $variable('labelIRI')),
+      $quad($variable('labelIRI'), rdfs.label, $variable('label'))
+    ]).map(binding => (binding.label as Term).value);
+    if (!haveSameStrings(labels, this.labels)) return false;
+
+    const properties = extractPropertyNameLabels(dataset, element);
+    if (!haveSameProperties(properties, this.properties)) return false;
+
+    return true;
+  }
+
+  static nodeCompliesWith(dataset: DStar, element: Quad_Subject, predicate: NamedNode, targetSchema: Quad_Subject | null) {
+    if (targetSchema === null) return true;
+
+    const theNode = followThrough(dataset, element, predicate);
+    if (theNode === null) return false;
+
+    const theSchema = followThrough(dataset, theNode as Quad_Subject, prec._prsc_schema);
+    if (theSchema === null) return false;
+
+    return targetSchema.equals(theSchema);
+  }
+
+  instanciate(source: DStar, element: Quad_Subject, quadToAdd: Quad): Quad {
+    const transform = (term: Term): Term => {
+      if (term.termType === 'Quad') {
+        return $quad(
+          transform(term.subject) as Quad_Subject,
+          transform(term.predicate) as Quad_Predicate,
+          transform(term.object) as Quad_Object,
+        );
+      }
+
+      if (term.equals(pvar.edge)) return element;
+      if (term.equals(pvar.source))      return followThrough(source, element, rdf.subject)!;
+      if (term.equals(pvar.destination)) return followThrough(source, element, rdf.object )!;
+      if (term.termType === 'BlankNode') {
+        return instanciateBlankNode(this.contextGraph, source, term, element);
+      }
+      return term;
+    };
+
+    return transform(quadToAdd) as Quad;
   }
 }
 
-//class EdgeSchemaDetector {
-//
-//}
+function instanciateBlankNode(contextGraph: DStar, dataGraph: DStar, term: Term, element: Quad_Subject) {
+  const triples = contextGraph.match(term);
+  if (triples.size !== 1) throw Error('Blank node is not unique ; ' + triples.size);
+  const triple = [...triples][0];
+  if (triple.predicate.equals(prec.prsc_valueOf)) {
+    if (triple.object.termType !== 'Literal')
+      throw Error('Object is not a literal');
+
+    const propertyName = triple.object.value;
+    const bindings = dataGraph.matchAndBind([
+      $quad(element, $variable('propertyKey'), $variable('propertyBN')),
+      $quad($variable('propertyKey'), rdfs.label, $literal(propertyName)),
+      $quad($variable('propertyBN'), rdf.value, $variable('propertyValue'))
+    ]).map(bindings => bindings.propertyValue as Term);
+    
+    if (bindings.length !== 1) throw Error('Not 1 value');
+    return bindings[0];
+  } else {
+    throw Error('Found bad blank node');
+  }
+}
 
 class PrscContext {
   nodeSchema: SchemaDetector[] = [];
@@ -133,9 +249,9 @@ class PrscContext {
       this.nodeSchema.push(new NodeSchemaDetector(dataset, nodeForm.subject));
     }
 
-    //for (const edgeForm of dataset.match(null, rdf.type, prec.prsc_edge, $defaultGraph)) {
-    //  this.edgeSchema.push(new EdgeSchemaDetector(dataset, nodeForm));
-    //}
+    for (const edgeForm of dataset.match(null, rdf.type, prec.prsc_edge, $defaultGraph)) {
+      this.edgeSchema.push(new EdgeSchemaDetector(dataset, edgeForm.subject));
+    }
   }
 
   markSchemaOfNodes(dataset: DStar, output: DStar) {
@@ -159,19 +275,17 @@ class PrscContext {
   }
 
   markSchemaOfEdges(dataset: DStar, output: DStar) {
-    dataset;
-    output;
-    return true;
-    /*
-    for (const edge of dataset.match(null, rdf.type, pgo.Node, $defaultGraph)) {
+    for (const edge of dataset.match(null, rdf.type, pgo.Edge, $defaultGraph)) {
       const schema = this.findEdgeSchema(dataset, edge.subject);
-      if (schema === null) return false;
+      if (schema === null) {
+        console.error("No schema for edge " + edge.subject.value);
+        return false;
+      }
       dataset.add($quad(edge.subject, prec._prsc_schema, schema.iri));
       schema.produce(output, dataset, edge.subject);
     }
 
     return true;
-    */
   }
 
   findEdgeSchema(dataset: DStar, edge: Quad_Subject) {
