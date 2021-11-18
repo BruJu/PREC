583ba0a57722442461ed2197c0a033960fe7e30c
Julian Bruyat <bruyat.ju@live.fr>
2021-11-18 23:20:30 +0100

    Well Behaved PRSC context: baseline code for detection + unit test for element identification

---
+++ src/prsc/WellBehavedCheck.ts
@@ -0,0 +1,178 @@
+import TermMap from '@rdfjs/term-map';
+import TermSet from "@rdfjs/term-set";
+import * as RDF from "@rdfjs/types";
+import {
+  characterizeTemplateTriple,
+  haveSameStrings, PRSCRule, PRSCSchema
+} from '../prec-c/PrscContext';
+import { precValueOf, pvarDestination, pvarEdge, pvarNode, pvarSelf, pvarSource } from "../prec/PRECNamespace";
+import * as QuadStar from '../rdf/quad-star';
+
+
+/** A violation for a Well Behaved Context */
+export type WellBehavedViolation = {
+  rule: PRSCRule,
+  reason: string
+};
+
+/**
+ * Checks if pvar:self is in every triple of the template
+ * @param rule The rule
+ * @returns True if pvar:self is in every triple of the template
+ */
+export function elementIdentification(rule: PRSCRule): boolean {
+  const other = rule.type === 'node' ? pvarNode : pvarEdge;
+  return undefined === rule.template.find(templateTriple =>
+    !(QuadStar.containsTerm(templateTriple, pvarSelf)
+    || QuadStar.containsTerm(templateTriple, other))
+  );
+}
+
+/**
+ * Return the list of rules that do not have a signature template i.e. those
+ * that we think can not produce triples that only them can produce.
+ * @param rules List of rules 
+ * @returns The list of rules that do not have a signature template
+ */
+export function signatureTriple(rules: PRSCRule[]): PRSCRule[] {
+  // 1) Build a map characterization -> rule
+  const found = new TermMap<RDF.Quad, PRSCRule | null>();
+
+  rules.forEach(rule => rule.template.forEach(templateTriple => {
+    const characterized = characterizeTemplateTriple(templateTriple);
+
+    const f = found.get(characterized);
+    if (f === undefined) {
+      found.set(characterized, rule);
+    } else if (f === rule) {
+      // ok: multiple signature templates within the same rule can produce the
+      // same triples
+    } else if (f !== null) {
+      // not ok: This template triple is shared by several rules
+      found.set(characterized, null);
+    } else {
+      // f === null, we know this template triple is not signature
+    }
+  }));
+
+  // 2) Find which rules are in the map
+  const withSignature = new TermSet<RDF.Term>();
+
+  for (const ruleOrNull of found.values()) {
+    if (ruleOrNull !== null) {
+      withSignature.add(ruleOrNull.identity);
+    }
+  }
+
+  // 3) Build list of missings and return it
+  return rules.filter(rule => !withSignature.has(rule.identity));
+}
+
+enum SpecialArg { Source, Destination }
+
+/**
+ * Checks if no value can be lost during the PG to RDF conversion
+ * @param rule The rule
+ * @returns True if no value can be lost during the PG to RDF conversion
+ */
+export function noValueLoss(rule: PRSCRule): boolean {
+  let m = new TermMap<RDF.Quad, null | { quad: RDF.Quad, values: (string | SpecialArg)[] }>();
+
+  for (const templateTriple of rule.template) {
+    const args = extractArgs(templateTriple);
+
+    if (args.length !== 0) {
+      const characterized = characterizeTemplateTriple(templateTriple);
+      const mappedTo = m.get(characterized);
+      if (mappedTo === undefined) {
+        m.set(characterized, { quad: templateTriple, values: args });
+      } else if (mappedTo !== null) {
+        m.set(characterized, null);
+      }
+    }
+  }
+
+  let foundSrc = false;
+  let foundDest = false;
+  let foundPropertyValues = new Set<string>();
+
+  for (const uniqueProperties of m.values()) {
+    if (uniqueProperties === null) continue;
+
+    const { values } = uniqueProperties;
+    for (const value of values) {
+      if (value === SpecialArg.Source) foundSrc = true;
+      else if (value === SpecialArg.Destination) foundDest = true;
+      else foundPropertyValues.add(value);
+    }
+  }
+
+  if (foundSrc !== foundDest) return false;
+  if (foundSrc !== (rule.type === 'edge')) return false;
+  if (!haveSameStrings([...foundPropertyValues], rule.properties)) return false;
+  return true;
+}
+
+
+/** Extract the list of placeholders in the template triple */
+function extractArgs(templateTriple: RDF.Quad): (string | SpecialArg)[] {
+  let result: (string | SpecialArg)[] = [];
+
+  function visit(term: RDF.Term) {
+    if (term.termType === 'Quad') {
+      visit(term.subject);
+      visit(term.predicate);
+      visit(term.object);
+      visit(term.graph);
+    } else if (term.termType === 'NamedNode') {
+      if (term.equals(pvarSource)) result.push(SpecialArg.Source);
+      if (term.equals(pvarDestination)) result.push(SpecialArg.Source);
+    } else if (term.termType === 'Literal') {
+      if (term.datatype.equals(precValueOf)) {
+        result.push(term.value);
+      }
+    }
+  }
+
+  visit(templateTriple);
+
+  return result;
+}
+
+/** Adds a violation in the list of violation */
+function addViolation(map: WellBehavedViolation[], rule: PRSCRule, message: string) {
+  const it = map.find(violation => violation.rule === rule);
+  if (it === undefined) {
+    map.push({ rule: rule, reason: message });
+  } else {
+    it.reason += " / " + message;
+  }
+}
+
+/**
+ * Check if the context is well behaved, i.e. if we have proved in a WIP paper
+ * that it is reversible
+ * @param context The context
+ * @returns True if the context is well behaved, else the list of detected
+ * violations
+ */
+export default function wellBehavedCheck(context: PRSCSchema): true | WellBehavedViolation[] {
+  let violations: WellBehavedViolation[] = [];
+  
+  for (const rule of context.prscRules) {
+    if (!elementIdentification(rule)) {
+      addViolation(violations, rule, "pvar:self is not in every triple");
+    }
+
+    if (!noValueLoss(rule)) {
+      addViolation(violations, rule, "Some data from the Property Graph is lost");
+    }
+  }
+
+  const rulesWithoutSignature = signatureTriple(context.prscRules);
+  rulesWithoutSignature.forEach(ruleWithoutSignature =>
+    addViolation(violations, ruleWithoutSignature, "No signature")
+  );
+
+  return violations.length === 0 ? true : violations;
+}
---
+++ test/WellBehavedCheck.test.ts
@@ -0,0 +1,138 @@
+import assert from 'assert';
+
+import path from 'path';
+import * as RDF from '@rdfjs/types';
+import * as N3 from 'n3';
+import fs from 'fs';
+import { PRSCSchema } from '../src/prec-c/PrscContext';
+import TermMap from '@rdfjs/term-map';
+import * as RDFString from 'rdf-string';
+
+import * as WBC from '../src/prsc/WellBehavedCheck';
+
+import namespace from '@rdfjs/namespace';
+import { xsdBoolToBool } from '../src/rdf/utils';
+const prec = namespace("http://bruy.at/prec#"                       , { factory: N3.DataFactory });
+const thisns = namespace("http://bruy.at/prec#name=", { factory: N3.DataFactory });
+
+describe('WellBehavedCheck', () => {
+  const tests = readResourceFile(path.join(__dirname, 'WellBehavedCheck.ttl'));
+
+  for (const test of tests) {
+    it(RDFString.termToString(test.name), () => {
+      const schema = new PRSCSchema(test.quads);
+
+      for (const [ident, conditions] of test.testRules) {
+        if (conditions.elementIdentification !== undefined) {
+          const rule = schema.prscRules.find(r => r.identity.equals(ident));
+          assert.ok(rule !== undefined);
+          const r = WBC.elementIdentification(rule);
+          assert.ok(
+            r === conditions.elementIdentification,
+            RDFString.termToString(ident)
+            + " is"
+            + (conditions.elementIdentification ? "" : " not")
+            + " expected to comply with the element identification criteria"
+          );
+        }
+
+        if (conditions.noValueLoss !== undefined) {
+          const rule = schema.prscRules.find(r => r.identity.equals(ident));
+          assert.ok(rule !== undefined);
+          const r = WBC.noValueLoss(rule);
+          assert.ok(
+            r === conditions.noValueLoss,
+            RDFString.termToString(ident)
+            + " is"
+            + (conditions.noValueLoss ? "" : " not")
+            + " expected to comply with the no value loss criteria"
+          );
+        }
+      }
+
+      if (test.signatureTriple !== undefined) {
+        assert.ok(
+          (WBC.signatureTriple(schema.prscRules).length === 0) === test.signatureTriple,
+          "The context should have "
+          + (test.signatureTriple ? "all rules with " : "some rules without ")
+          + "a signature"
+        );
+      }
+
+      if (test.isWellBehaved !== undefined) {
+        assert.ok(
+          WBC.default(schema) === test.isWellBehaved,
+          "The context should " + ( test.isWellBehaved ? "" : "not " )
+          + "be considered well behaved"
+        );
+      }
+    });
+  }
+});
+
+
+type TestGraph = {
+  name: RDF.Term;
+  quads: RDF.Quad[];
+  isWellBehaved?: boolean;
+  signatureTriple?: boolean;
+  testRules: TermMap<RDF.Term, TestRule>;
+};
+
+type TestRule = {
+  elementIdentification?: boolean;
+  noValueLoss?: boolean;
+};
+
+function readResourceFile(path: string): TestGraph[] {
+  const content = fs.readFileSync(path, 'utf-8');
+  const quads = new N3.Parser().parse(content);
+  
+  const testGraphs = new TermMap<RDF.Term, TestGraph>();
+
+  quads.forEach(quad => {
+    const graph = quad.graph;
+    if (graph.equals(N3.DataFactory.defaultGraph())) return;
+
+    let x: TestGraph | undefined = testGraphs.get(graph);
+    if (x === undefined) {
+      x = { name: graph, quads: [], testRules: new TermMap() };
+      testGraphs.set(graph, x);
+    }
+
+    if (quad.subject.equals(thisns.all)) {
+      if (quad.predicate.equals(prec.well_behaved)) {
+        x.isWellBehaved = xsdBoolToBool(quad.object);
+      } else 
+      if (quad.predicate.equals(prec.signature)) {
+        x.signatureTriple = xsdBoolToBool(quad.object);
+      } else {
+        throw Error("Unsupported quad: " + RDFString.termToString(quad));
+      }
+    } else {
+      if (quad.predicate.equals(prec.element_identification)) {
+        let y = x.testRules.get(quad.subject);
+        if (y === undefined) {
+          y = {};
+          x.testRules.set(quad.subject, y);
+        }
+
+        y.elementIdentification = xsdBoolToBool(quad.object);
+      } else if (quad.predicate.equals(prec.noValueLoss)) {
+        let y = x.testRules.get(quad.subject);
+        if (y === undefined) {
+          y = {};
+          x.testRules.set(quad.subject, y);
+        }
+
+        y.noValueLoss = xsdBoolToBool(quad.object);
+      } else {
+        x.quads.push(N3.DataFactory.quad(
+          quad.subject, quad.predicate, quad.object
+        ));
+      }
+    }
+  });
+
+  return [...testGraphs.values()];
+}
