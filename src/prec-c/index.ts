8be17c250d27774234947912e4ac3208ca5f5dea
Julian Bruyat <julian@bruy.at>
2024-09-17 15:45:14 +0200

    PREC-C: Move out of general apply-context files PREC-C specific code

---
+++ src/prec-c/index.ts
@@ -0,0 +1,125 @@
+import { DataFactory } from 'n3';
+import DStar from "../dataset";
+
+import * as QuadStar from '../rdf/quad-star';
+import TermSet from '@rdfjs/term-set';
+import * as RDF from "@rdfjs/types";
+import Context from "./Context";
+
+import { rdf, pgo, prec, $defaultGraph } from '../PRECNamespace';
+
+/**
+ * Given a PREC-0 PG and a list of triples that descrives a PREC-C context, transform
+ * the dataset that stores the PREC-0 PG into an idiomatic RDF graph
+ * @param dataset The PREC-0 property graph that will be transformed into an idiomatic
+ * RDF graph
+ * @param contextQuads The triples in the context
+ */
+export default function precC(dataset: DStar, contextQuads: RDF.Quad[]) {
+  const context = new Context(contextQuads);
+
+  // -- Blank nodes transformation
+  for (let typeOfNode in context.blankNodeMapping) {
+    blankNodeMapping(
+      dataset,
+      DataFactory.namedNode(typeOfNode),
+      context.blankNodeMapping[typeOfNode]
+    );
+  }
+
+  // -- Map generated IRI to existing IRIs + apply the templates
+  const newDataset = ruleBasedProduction(dataset, context);
+  dataset.deleteMatches();
+  dataset.addAll(newDataset.getQuads());
+
+  // -- Remove provenance information if they are not required by the user
+  if (context.keepProvenance === false) removePGO(dataset);
+}
+
+
+/**
+ * Build a new dataset by translating the PREC-0 triples in the source `dataset`
+ * according to the rules described in the passed `context`.
+ * @param dataset The source dataset
+ * @param context The context
+ * @returns The new dataset 
+ */
+function ruleBasedProduction(dataset: DStar, context: Context): DStar {
+  context.produceMarks(dataset);
+  
+  const newDataset = new DStar();
+
+  const preservedLabels = new TermSet<RDF.Term>();
+  
+  for (const nepManager of context.entityManagers) {
+    const ruleType = nepManager.ruleset;
+
+    for (const mark of dataset.getQuads(null, ruleType.mark, null, $defaultGraph)) {
+      const ts = ruleType.applyMark(newDataset, mark, dataset, context);
+      ts.forEach(t => preservedLabels.add(t));
+    }
+  }
+
+  preservedLabels.forEach(label => newDataset.addAll(dataset.getQuads(label)));
+  
+  // As it is impossible to write a rule that catches a node without any label
+  // and property, we add back the nodes here
+  newDataset.addAll(dataset.getQuads(null, rdf.type, pgo.Node, $defaultGraph));
+
+  return newDataset;
+}
+
+
+// ==== Blank Node Mapping
+
+/**
+ * Transform the blank nodes of the given type to named nodes, by appending to
+ * the given prefix the current name of the blank node.
+ * @param dataset The dataset that contains the quads
+ * @param typeOfMappedNodes The type of the IRIs to map
+ * @param prefixIRI The prefix used
+ */
+function blankNodeMapping(dataset: DStar, typeOfMappedNodes: RDF.Term, prefixIRI: string) {
+  let remapping: {[blankNodeName: string]: RDF.NamedNode} = {};
+
+  dataset.getQuads(null, rdf.type, typeOfMappedNodes, $defaultGraph)
+    .filter(quad => quad.subject.termType === "BlankNode")
+    .map(quad => quad.subject.value)
+    .forEach(blankNodeValue => remapping[blankNodeValue] = DataFactory.namedNode(prefixIRI + blankNodeValue))
+  
+  let newContent = dataset.getQuads().map(quad => _quadBNMap(remapping, quad));
+  
+  dataset.removeQuads(dataset.getQuads());
+  dataset.addAll(newContent);
+}
+
+/**
+ * Provided a mapping blank node value => named node, maps the quad to another
+ * quad, in which every blank node in the mapping is mapped to the named node
+ */
+function _quadBNMap(map: {[blankNodeName: string]: RDF.NamedNode}, quad: RDF.Quad): RDF.Quad {
+  return QuadStar.eventuallyRebuildQuad(quad, term => {
+    if (term.termType === "BlankNode") {
+      let mappedTo = map[term.value];
+      if (mappedTo === undefined) return term;
+      return mappedTo;
+    } else {
+      return term;
+    }
+  });
+}
+
+/**
+ * Deletes all occurrence of pgo:Edge pgo:Node, prec:PropertyKey and prec:PropertyKeyValue.
+ * 
+ * While the PGO ontology is usefull to describe the PG structure, and to
+ * specify the provenance of the data, some user may want to discard these
+ * provenance information.
+ * @param dataset The dataset 
+ */
+function removePGO(dataset: DStar) {
+  dataset.deleteMatches(null, rdf.type, pgo.Edge             , $defaultGraph);
+  dataset.deleteMatches(null, rdf.type, pgo.Node             , $defaultGraph);
+  dataset.deleteMatches(null, rdf.type, prec.PropertyKey     , $defaultGraph);
+  dataset.deleteMatches(null, rdf.type, prec.PropertyKeyValue, $defaultGraph);
+}
--- src/prec/apply-context.ts
+++ src/prec/apply-context.ts
@@ -1,18 +1,18 @@
-import { DataFactory } from 'n3';
 import DStar from '../dataset/index';
 
-import Context, { allPRECCExclusiveTerms } from "../prec-c/Context";
-import * as QuadStar from '../rdf/quad-star';
+import { allPRECCExclusiveTerms } from "../prec-c/Context";
 import TermSet from '@rdfjs/term-set';
 import * as RDF from "@rdfjs/types";
 
-import { rdf, pgo, prec, $defaultGraph, pvar } from '../PRECNamespace';
+import { rdf, prec, pvar } from '../PRECNamespace';
 
 import prsc from '../prsc';
 import { termToString } from 'rdf-string';
+import precC from '../prec-c';
 
 // =============================================================================
 
+/** List of context types */
 export enum ContextType { PRECC, PRSC, Empty, Invalid };
 
 /**
@@ -93,6 +93,12 @@ function hasForbiddenTerms(quads: RDF.Quad[]): RDF.Term[] | null {
   return null;
 }
 
+/**
+ * Return the type of the context described by the triples: PREC-C, PRSC,
+ * empty or invalid.
+ * @param contextQuads The context
+ * @returns The type of the context
+ */
 export function getContextType(contextQuads: RDF.Quad[]): ContextType {
   if (contextQuads.length === 0) return ContextType.Empty;
 
@@ -151,114 +157,5 @@ export function applyPRSC(dataset: DStar, contextQuads: RDF.Quad[], obsoleteAreC
  * @param contextQuads The PREC-C context
  */
 export function applyPRECC(dataset: DStar, contextQuads: RDF.Quad[]) {
-  const context = new Context(contextQuads);
-
-  // -- Blank nodes transformation
-  for (let typeOfNode in context.blankNodeMapping) {
-    blankNodeMapping(
-      dataset,
-      DataFactory.namedNode(typeOfNode),
-      context.blankNodeMapping[typeOfNode]
-    );
-  }
-
-  // -- Map generated IRI to existing IRIs + apply the templates
-  const newDataset = ruleBasedProduction(dataset, context);
-  dataset.deleteMatches();
-  dataset.addAll(newDataset.getQuads());
-
-  // -- Remove provenance information if they are not required by the user
-  if (context.keepProvenance === false) removePGO(dataset);
-}
-
-/**
- * Build a new dataset by translating the PREC-0 triples in the source `dataset`
- * according to the rules described in the passed `context`.
- * @param dataset The source dataset
- * @param context The context
- * @returns The new dataset 
- */
-function ruleBasedProduction(dataset: DStar, context: Context): DStar {
-  context.produceMarks(dataset);
-  
-  const newDataset = new DStar();
-
-  const preservedLabels = new TermSet<RDF.Term>();
-  
-  for (const entityManager of context.entityManagers) {
-    const ruleType = entityManager.ruleset;
-
-    for (const mark of dataset.getQuads(null, ruleType.mark, null, $defaultGraph)) {
-      const ts = ruleType.applyMark(newDataset, mark, dataset, context);
-      ts.forEach(t => preservedLabels.add(t));
-    }
-  }
-
-  preservedLabels.forEach(label => newDataset.addAll(dataset.getQuads(label)));
-  
-  // As it is impossible to write a rule that catches a node without any label
-  // and property, we add back the nodes here
-  newDataset.addAll(dataset.getQuads(null, rdf.type, pgo.Node, $defaultGraph));
-
-  return newDataset;
-}
-
-// =============================================================================
-// =============================================================================
-
-// ==== Blank Node Mapping
-
-/**
- * Transform the blank nodes of the given type to named nodes, by appending to
- * the given prefix the current name of the blank node.
- * @param dataset The dataset that contains the quads
- * @param typeOfMappedNodes The type of the IRIs to map
- * @param prefixIRI The prefix used
- */
-function blankNodeMapping(dataset: DStar, typeOfMappedNodes: RDF.Term, prefixIRI: string) {
-  let remapping: {[blankNodeName: string]: RDF.NamedNode} = {};
-
-  dataset.getQuads(null, rdf.type, typeOfMappedNodes, $defaultGraph)
-    .filter(quad => quad.subject.termType === "BlankNode")
-    .map(quad => quad.subject.value)
-    .forEach(blankNodeValue => remapping[blankNodeValue] = DataFactory.namedNode(prefixIRI + blankNodeValue))
-  
-  let newContent = dataset.getQuads().map(quad => _quadBNMap(remapping, quad));
-  
-  dataset.removeQuads(dataset.getQuads());
-  dataset.addAll(newContent);
-}
-
-/**
- * Provided a mapping blank node value => named node, maps the quad to another
- * quad, in which every blank node in the mapping is mapped to the named node
- */
-function _quadBNMap(map: {[blankNodeName: string]: RDF.NamedNode}, quad: RDF.Quad): RDF.Quad {
-  return QuadStar.eventuallyRebuildQuad(quad, term => {
-    if (term.termType === "BlankNode") {
-      let mappedTo = map[term.value];
-      if (mappedTo === undefined) return term;
-      return mappedTo;
-    } else {
-      return term;
-    }
-  });
-}
-
-
-// ==== AG
-
-/**
- * Deletes every occurrence of pgo:Edge pgo:Node, prec:PropertyKey and prec:PropertyKeyValue.
- * 
- * While the PGO ontology is usefull to describe the PG structure, and to
- * specify the provenance of the data, some user may want to discard these
- * provenance information.
- * @param dataset The dataset 
- */
-function removePGO(dataset: DStar) {
-    dataset.deleteMatches(null, rdf.type, pgo.Edge             , $defaultGraph);
-    dataset.deleteMatches(null, rdf.type, pgo.Node             , $defaultGraph);
-    dataset.deleteMatches(null, rdf.type, prec.PropertyKey     , $defaultGraph);
-    dataset.deleteMatches(null, rdf.type, prec.PropertyKeyValue, $defaultGraph);
+  precC(dataset, contextQuads);
 }
